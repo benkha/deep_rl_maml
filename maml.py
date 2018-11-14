4f1f597a23a804dbed0875173bca2b81429b49da
Nikhil <ennsharma@berkeley.edu>
2018-11-14 03:24:32 -0800

    Checkpointing

--- maml.py
+++ maml.py
@@ -4,15 +4,6 @@ import numpy as np
 import time
 from sonic_util import make_env, sample_env
 
-
-def build_mlp(x, output_size, scope, n_layers, size, activation=tf.tanh, output_activation=None, regularizer=None):
-    i = 0
-    for i in range(n_layers):
-        x = tf.layers.dense(inputs=x,units=size, activation=activation, name='fc{}'.format(i), kernel_regularizer=regularizer, bias_regularizer=regularizer)
-
-    x = tf.layers.dense(inputs=x, units=output_size, activation=output_activation, name='fc{}'.format(i + 1), kernel_regularizer=regularizer, bias_regularizer=regularizer)
-    return x
-
 def setup_logger(logdir, locals_):
     # Configure output directory for logging
     logz.configure_output_dir(logdir)
@@ -23,7 +14,6 @@ def setup_logger(logdir, locals_):
 
 class MAML(object):
 	def __init__(self, computation_graph_args):
-		self.n_layers = computation_graph_args['n_layers']
 		self.alpha = computation_graph_args['alpha']
 		self.beta = computation_graph_args['beta']
 		self.ob_dim = computation_graph_args['ob_dim']
@@ -31,14 +21,17 @@ class MAML(object):
 		self.batch_size = computation_graph_args['batch_size']
 		self.horizon = computation_graph_args['horizon']
 		self.n_trajectories = computation_graph_args['n_trajectories']
-        self.meta_model_path = computation_graph_args['meta_model_path']
+        self.is_train = computation_graph_args['is_train']
+        self.model_type = computation_graph_args['model_type']
+        self.loss_type = computation_graph_args['loss_type']
 
-        self.replay_buffer = ReplayBuffer(
-            computation_graph_args['replay_buffer_size'], 
-            computation_graph_args['frame_history_len']
-        )
+        self.meta_train_obs, self.meta_train_ac, self.meta_val_obs, self.meta_val_ac \
+            = self.define_placeholders()
 
-        self.model_saver = tf.train.Saver()
+        self.meta_optimizer = tf.train.AdamOptimizer(self.beta)
+        self.model = self.import_model()
+        self.loss_fn = self.import_loss_fn()
+        self.build_computation_graph()
 
 	def init_tf_sess(self):
 		tf_config = tf.ConfigProto(inter_op_parallelism_threads=1, intra_op_parallelism_threads=1)
@@ -48,71 +41,63 @@ class MAML(object):
         tf.global_variables_initializer().run()
 
     def define_placeholders(self):
-    	sy_ob_no = tf.placeholder(shape=[None, self.ob_dim], name="ob", dtype=tf.float32)
-        sy_sampled_ac = tf.placeholder(shape=[None, self.ac_dim], name="ac", dtype=tf.float32)
+    	meta_train_obs = tf.placeholder(shape=[None, self.ob_dim], name="train_ob", dtype=tf.float32)
+        meta_train_ac = tf.placeholder(shape=[None, self.ac_dim], name="train_ac", dtype=tf.float32)
+        meta_val_obs = tf.placeholder(shape=[None, self.ob_dim], name="val_ob", dtype=tf.float32)
+        meta_val_ac = tf.placeholder(shape=[None, self.ac_dim], name="val_ac", dtype=tf.float32)
 
     def build_computation_graph(self):
         self.loss = 0.0 # TODO: build full computation graph, I think this should depend on using a MLP to sample actions not sure
     	self.optimizer = tf.train.AdamOptimizer(self.alpha).minimize(self.loss)
 
-    def build_meta_computation_graph(self):
-        self.meta_loss = tf.reduce_sum(trajectory_losses) / tf.to_float(self.batch_size)
-        self.meta_optimizer = tf.train.AdamOptimizer(self.beta).minimize(self.meta_loss)
+    def import_model(self):
+        if self.model_type == 'fc':
+            import model.fc as model
+        else:
+            ValueError("Can't recognize the model type {}".format(self.model_type))
+        return model
 
-    def meta_step(self, learned_policies):
-        # TODO: figure out how to pass in learned policies to feed_dict in the best way
-        # the loss function uses the sum of the learned policies' individual losses to take a step
-    	self.sess.run(self.meta_theta, feed_dict={})
+    def import_loss_fn(self):
+        if self.loss_type == 'mse':
+            self.loss_fn = tf.losses.mean_squared_error
+        else:
+            ValueError("Can't recognize the loss type {}".format(self.loss_type))
 
-    def sample_trajectories(self, env, for_meta):
-        for _ in range(self.n_trajectories):
-            self.sample_trajectory(env, for_meta=for_meta)
+    def metalearn(self):
+        pass
 
-    def sample_trajectory(self, env, for_meta):
-    	obs = env.reset()
-    	for i in range(self.horizon):
-    		# Select action using policy
-    		ac = self.sess.run(self.sy_sampled_ac, feed_dict={self.sy_ob_no: obs})
+    def evaluate(self):
+        pass
 
-    		# Step the environment
-    		obs, rew, done, _ = env.step(ac)
+    def train_step(self):
+        pass
 
-    		# Add to replay buffer
-            self.replay_buffer.store() # TODO
-
-    		if done:
-    			obs = env.reset()
-
-    def load_meta_model(self):
-        self.model_saver.restore(self.sess, self.meta_model_path)
-
+    def test_step(self):
+        pass
 
 def train_MAML(
 		env_name,
 		exp_name,
+        model_type,
+        loss_type,
 		render,
 		n_iter
 		alpha,
 		beta,
 		batch_size,
 		horizon,
-		n_trajectories
-		logdir
+		n_trajectories,
+        seed,
+        is_train
 		):
-	# Initialize clock
-	start = time.time()
-
-	# Setup logger
-	setup_logger(logdir, locals())
-
 	# Initialize environment
+    np.random.seed(seed)
 	env = sample_env()
     env.reset()
     ob_dim = env.observation_space.shape[0]
     ac_dim = env.action_space.shape[0]
 
 	computation_graph_args = {
-        'n_layers': n_layers,
         'alpha': alpha,
         'beta': beta,
         'ob_dim': ob_dim
@@ -120,30 +105,18 @@ def train_MAML(
         'batch_size': batch_size
         'horizon': horizon
         'n_trajectories': n_trajectories
-        'replay_buffer_size': 1000000
-        'frame_history_len': 4
+        'is_train': is_train
+        'model_type': model_type
+        'loss_type': loss_type
     }
 
     # Initialize metalearner
     maml_agent = MAML(computation_graph_args)
-    maml_agent.build_computation_graph()
-    maml_agent.build_meta_computation_graph()
     maml_agent.init_tf_sess()
-
-    for i in range(n_iter):
-    	print("********** Iteration %i ************"%itr)
-    	# TODO: replace num_tasks with number of training levels
-    	envs = [sample_env() for _ in range(batch_size)]
-    	for env in envs:
-    		maml_agent.sample_trajectories(env, for_meta=False)
-
-	    	# TODO: Extract trajectories from replay buffer
-	    	meta_model = maml_agent.get_meta_model()
-
-            maml_agent.sample_trajectories(task, for_meta=True)
-
-        # TODO: pass learned policies to the meta step somehow
-	    agent.meta_step()
+    if is_train:
+        maml_agent.metalearn() # TODO: insert max steps?
+    else:
+        maml_agent.evaluate() # TODO: params
 
 
 def main():
@@ -151,6 +124,8 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('env_name', type=str)
     parser.add_argument('--exp_name', type=str, default='vac')
+    parser.add_argument('--model_type', type=str, default='fc')
+    parser.add_argument('--loss_type', type=str, default='mse')
     parser.add_argument('--render', action='store_true')
     parser.add_argument('--n_iter', '-n', type=int, default=100)
     parser.add_argument('--alpha', type=float, default=1.0)
@@ -158,6 +133,8 @@ def main():
     parser.add_argument('--batch_size', '-b', type=int, default=1)
     parser.add_argument('--horizon', '-h', type=int, default=100)
     parser.add_argument('--n_trajectories', '-t', type=int, default=10)
+    parser.add_argument('--seed', '-s', type=int, default=1)
+    parser.add_argument('--is_train', action='store_true')
 	args = parser.parse_args()
 
 	data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -172,6 +149,8 @@ def main():
 	train_MAML(
 		env_name=args.env_name,
 		exp_name=args.exp_name,
+        model_type=args.model_type,
+        loss_type=args.loss_type,
 		render=args.render,
 		n_iter=args.n_iter,
 		alpha=args.alpha,
@@ -179,7 +158,9 @@ def main():
 		batch_size=args.batch_size,
 		logdir=logdir,
 		horizon=horizon,
-		n_trajectories=n_trajectories
+		n_trajectories=n_trajectories,
+        seed=args.seed,
+        is_train=args.is_train
 	)
 
 if __name__ == '__main__':
